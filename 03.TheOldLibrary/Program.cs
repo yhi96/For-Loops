0bbb83ce4cdc96bdb11df12c5d306d96f992b797
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-04 10:54:58 +0200

    Add files via upload

---
+++ 03.TheOldLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _03.TheOldLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favorite = Console.ReadLine();
+            string current = Console.ReadLine();
+
+            int checkedBooks = 0;
+
+            while (current != "No More Books" && current != favorite)
+            {
+                checkedBooks += 1;
+                current = Console.ReadLine();
+            }
+            if (current == favorite)
+            {
+                Console.WriteLine($"You checked {checkedBooks} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {checkedBooks} books.");
+            }
+
+        }
+    }
+}
