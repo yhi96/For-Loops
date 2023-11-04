b44f43c39703c257d7ec431b098f7c10e217b0fe
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-04 10:53:09 +0200

    Add files via upload

---
+++ 10.NumberPyramid/01.NumberPyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int current = 1;
+            bool isBigger = false;
+
+            for (int rows = 1; rows <= n; rows += 1)
+            {
+                for (int cols = 1; cols <= n; cols += 1)
+                {
+                    if (current > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.WriteLine(current + " ");
+                    current += 1;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
