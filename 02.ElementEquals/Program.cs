2d9ebb722381658dd3a1e268019d49fe755f9c8d
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-04 10:54:36 +0200

    Add files via upload

---
+++ 02.ElementEquals/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02.ElementEquals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int max = int.MinValue; 
+
+            for (int c = 0; c < count; c += 1)
+            { 
+                int cur = int.Parse(Console.ReadLine());
+                sum += cur; 
+            
+            if (cur > max)
+            
+                max = cur;
+            }
+            sum -= max;
+            if (sum == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sum}");
+            }
+            else
+            {
+                int diff = Math.Abs(sum - max);
+                    Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+
+            }
+        }
+    }
+}
