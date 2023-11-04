3fdb1533964746aea9e32e5a1e8bdd8812923597
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-04 10:56:27 +0200

    Add files via upload

---
+++ 04.ExamPreparation/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _04.ExamPreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int poorGradesLimit = int.Parse(Console.ReadLine());
+
+            int numberPoorGrades = 0;
+            double gradesTotal = 0;
+            int gradesNum = 0;
+            string lastProblem = "";
+
+            while (numberPoorGrades < poorGradesLimit)
+            {
+                string currentProblem = Console.ReadLine();
+                if (currentProblem == "Enough")
+                    break;
+                lastProblem = currentProblem;
+                int currentGrade = int.Parse(Console.ReadLine());
+
+                gradesTotal += currentGrade;
+                gradesNum += 1;
+
+                if (currentGrade <= 4)
+                    numberPoorGrades += 1;
+            }
+            if (numberPoorGrades >= poorGradesLimit)
+            {
+                Console.WriteLine($"You need a break, {numberPoorGrades} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {gradesTotal/gradesNum:f2}");
+                Console.WriteLine($"Number of problems: {gradesNum}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+        }
+    }
+}
