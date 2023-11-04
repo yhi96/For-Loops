f24c9f60b350eb2b5917f5d4c8eef78c85753dc5
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-04 10:54:05 +0200

    Add files via upload

---
+++ 01.Numbers1000/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Numbers1000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 997; i += 1)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
