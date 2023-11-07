ffd291dde971b7be42d4673af5856e7e2fdfd6c7
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-08 00:07:18 +0200

    Add files via upload

---
+++ 05.Vacation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class VacationSavings
+{
+    static void Main()
+    {
+        double moneyNeeded = double.Parse(Console.ReadLine());
+        double availableMoney = double.Parse(Console.ReadLine());
+
+        int consecutiveSpendDays = 0;
+        int totalDays = 0;
+
+        while (availableMoney < moneyNeeded && consecutiveSpendDays < 5)
+        {
+            string action = Console.ReadLine();
+            double amount = double.Parse(Console.ReadLine());
+
+            if (action == "save")
+            {
+                availableMoney += amount;
+                consecutiveSpendDays = 0;
+            }
+            else if (action == "spend")
+            {
+                availableMoney -= amount;
+                consecutiveSpendDays++;
+            }
+
+            if (availableMoney < 0)
+            {
+                availableMoney = 0;
+            }
+
+            totalDays++;
+        }
+
+        if (consecutiveSpendDays == 5)
+        {
+            Console.WriteLine("You can't save the money.");
+            Console.WriteLine(totalDays);
+        }
+        else
+        {
+            Console.WriteLine($"You saved the money for {totalDays} days.");
+        }
+    }
+}
