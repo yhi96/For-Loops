1a2eb20f0c34946a4a4d3a6a0b69c4f390ce4b00
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-08 00:08:48 +0200

    Add files via upload

---
+++ 08.BirthdayCake/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _08.BirthdayCake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeLength = int.Parse(Console.ReadLine());
+            
+
+            int cakeSize = cakeWidth * cakeLength;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "STOP")
+                {
+                    if (cakeSize >= 0)
+                    {
+                        Console.WriteLine($"{cakeSize} pieces are left.");
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{-cakeSize} pieces are left.");
+                    }
+                    break;
+                }
+
+                int piecesTaken = int.Parse(command);
+                cakeSize -= piecesTaken;
+                if (cakeSize < 0)
+                {
+                    int missingPieces = Math.Abs(cakeSize);
+                    Console.WriteLine($"No more cake left! You need {missingPieces} pieces more.");
+                    break;
+                }
+            }
+        }
+    }
+}
