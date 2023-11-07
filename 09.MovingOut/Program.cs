37db5c123beb17c0347cb70d1c20320e44c3e17d
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-08 00:09:10 +0200

    Add files via upload

---
+++ 09.MovingOut/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Design;
+
+namespace _09.MovingOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeSpaceWidth = int.Parse(Console.ReadLine());
+            int freeSpaceLength = int.Parse(Console.ReadLine());
+            int freeSpaceHeight = int.Parse(Console.ReadLine());
+
+            int freeSpace = freeSpaceWidth * freeSpaceLength * freeSpaceHeight;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Done")
+                {
+                    Console.WriteLine($"{freeSpace} Cubic meters left.");
+                    break;
+                }
+
+                int numberBoxes = int.Parse(command);
+
+                if (freeSpace >= numberBoxes)
+                {
+                    freeSpace -= numberBoxes;
+                }
+                else
+                {
+                    int neededSpace = numberBoxes - freeSpace;
+                    Console.WriteLine($"No more free space! You need {neededSpace} Cubic meters more.");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
