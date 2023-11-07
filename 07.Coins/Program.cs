5fe9361aca16ccd41d021a7cfde5d1cf5c71493c
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-08 00:08:15 +0200

    Add files via upload

---
+++ 07.Coins/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+namespace _07.Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] coins = { 2.00m, 1.00m, 0.50m, 0.20m, 0.10m, 0.05m, 0.02m, 0.01m };
+
+            Console.Write("");
+            decimal amount = decimal.Parse(Console.ReadLine());
+
+            int numberOfCoinsToBeReturned = 0;
+            int i = 0;
+
+            while (amount > 0 && i < coins.Length)
+            {
+                if (amount >= coins[i])
+                {
+                    amount -= coins[i];
+                    numberOfCoinsToBeReturned += 1;
+                }
+                else
+                {
+                    i += 1;
+                }
+            }
+
+            Console.WriteLine($"{numberOfCoinsToBeReturned}");
+        }
+    }
+}
